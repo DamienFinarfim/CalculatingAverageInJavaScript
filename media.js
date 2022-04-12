0334d30fddb4ac8d6f0116d89bcdefafc919e353
Alexandre Silva <alexandreasilva@protonmail.com>
2022-04-11 21:39:03 -0300

    Cálculo de média simples

--- media.js
+++ media.js
@@ -1,12 +1,24 @@
-function obterMedia(){
-
-    var a = parseInt(document.getElementById("a").value);
-    var b = parseInt(document.getElementById("b").value);
-    var c = parseInt(document.getElementById("c").value);
-
-    var media = (a + b + c) / 3;
-
-    document.getElementById("resultado").innerHTML = "Resultado:" + media;
-
-    console.log(a + b + c);
-}
+var calculator = new Vue({
+    el: '#calculator',
+    data: {
+        price: '',
+        downPayment: '',
+        tradeIn: '',
+        length: '60',
+        rate: '',
+        calcPayment: ''
+    },
+    computed: {
+        calcPayment: function(e){
+            var p = this.price - this.downPayment - this.tradeIn;
+            var r = this.rate / 1200;
+            var n = this.length;
+            var i = Math.pow((1+r),n);
+            var payment = ( p * r * i) / (i - 1) || 0;
+            return currencyFormat(payment);
+        },
+        numFormat: function(e){
+            e.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        }
+    }
+});
